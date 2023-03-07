c4e718b91a6b7519be39897eb08233fa6a5ab814
Emilio <a19300100@live.ceti.mx>
2023-03-07 16:52:07 -0600

    Third

---
+++ app/src/main/java/com/example/music/MusicListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.music;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MusicListAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+    }
+}
